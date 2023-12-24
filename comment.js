cec8f6fefba60006dfbb3a13165f2639265a7f35
timo9939 <timothylau20199@gmail.com>
2023-12-24 23:22:09 +0000

    extract section id by clicking reply button

--- comment.js
+++ comment.js
@@ -5,7 +5,7 @@ let after_second_section =document.getElementById("mainContainer")
 
 
 
-// Function of the submit button
+// Function of the mainsection submit button
 function submit(){
 
     let comment_content=document.getElementById("comment_content");
@@ -29,3 +29,38 @@ function submit(){
     document.getElementById("input").value= " ";
 }
 
+// Reply button and function
+let replyBtn=document.getElementsByClassName('replyBtn')
+let mainInput=document.getElementById("input-section")
+let replyinput=document.getElementById("reply-input")
+function pressReply(button){
+    // let number=button.id
+    // console.log(number)
+    // let commentNo=document.getElementById
+    console.log("replyBtn Clicked")
+    mainInput.style.display="none"
+    replyinput.style.display="block"
+
+ let GetSectionId = button.id;
+        console.log("Clicked button in section with ID: " + GetSectionId)
+
+// Append the Reply input below the specific comment
+
+}    
+
+function afterPressReply(GetSectionId){
+
+
+  let replyinputContent=document.getElementById("Replyinput").value
+    console.log("replyinputContent is ", replyinputContent)  
+
+        // Clone the template content
+        let clone = document.importNode(template.content, true);
+
+        // Update the cloned content with the input value
+        clone.querySelector("#comment_content").textContent = replyinputContent;
+console.log("GetSectinId is ",GetSectionId)
+        after_second_section.append(clone)
+
+       
+}
